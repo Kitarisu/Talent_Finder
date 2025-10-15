84cc199aa0ad4bf997c57de5595aab290969adb0
ahmedshiin <ahmed.benmalek69@gmail.com>
2025-10-15 13:14:01 +0200

    first commit

---
+++ src/test/java/com/talentfinder/ddc/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentfinder.ddc.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
