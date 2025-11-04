16cb735488c356529441660ea42f19d42fe26673
Kitarisu <Amilink123@gmail.com>
2025-11-04 19:15:34 +0100

    page d'acceuil qui MARCHE OMG !!!!!!!!!

---
+++ src/main/java/com/talentfinder/ddc/project/PageController.java
@@ -0,0 +1,28 @@
+package com.talentfinder.ddc.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/candidater")
+    public String candidater() {
+        return "candidater";
+    }
+
+    @GetMapping("/entreprises")
+    public String entreprises() {
+        return "entreprises";
+    }
+
+    @GetMapping("/candidatures")
+    public String candidatures() {
+        return "candidatures";
+    }
+}
